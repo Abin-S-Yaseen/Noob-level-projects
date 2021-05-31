bcca3ed52e40799a90b83f993e8d874e681ffa88
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-05-31 23:52:52 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,24 @@
+#Basic Calculator
+
+num1 = float(input('Enter the first number: '))
+optr = input('Enter the operator: ')
+num2 = float(input('Enter the second number and press Enter key: '))
+
+def add():
+    return num1 + num2
+def subtract():
+    return num1 - num2
+def multiply():
+    return num1 * num2
+def divide():
+    return num1 / num2
+
+if optr == '+':
+    print("The answer is " + str(add()))
+if optr == '-':
+    print("The answer is " + str(subtract()))
+if optr == '*':
+    print("The answer is " + str(multiply()))
+if optr == '/':
+    print("The answer is " + str(divide()))
+
