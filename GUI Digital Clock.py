fabec705f39b5602f9da624185426b14ebf2f5fd
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-06-13 14:13:00 +0530

    Create GUI Digital Clock.py

---
+++ GUI Digital Clock.py
@@ -0,0 +1,20 @@
+  
+from tkinter import Tk
+from tkinter import Label
+import time
+import sys
+
+screen = Tk()
+screen.title('Digital Clock')
+
+def get_time():
+    timeVar = time.strftime("%I:%M:%S %p")
+    clock.config(text=timeVar)
+    clock.after(200,get_time)
+
+clock = Label(screen,font=("calibri",90),bg="grey",fg="white")
+clock.pack()
+
+get_time()
+
+screen.mainloop()
