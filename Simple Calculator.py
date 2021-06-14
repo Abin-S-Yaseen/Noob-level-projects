53a74fd9789946ac5f900d8437a9783605c45426
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-06-14 10:31:47 +0530

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,27 +1,29 @@
 #Basic Calculator
 
-num1 = float(input('Enter the first number: '))
-optr = input('Enter the operator: ')
-num2 = float(input('Enter the second number and press Enter key: '))
-  
+while True:
+    print('BASIC CALCULATOR')
+    print('-----------------')
+    num1 = float(input('Enter the first number: '))
+    optr = input('Enter the operator: ')
+    num2 = float(input('Enter the second number: '))
+    def add(x,y):
+        return x + y
+    def subtract(x,y):
+        return x - y
+    def multiply(x,y):
+        return x * y
+    def divide(x,y):
+        return x / y
 
-def add():
-    return num1 + num2
-def subtract():
-    return num1 - num2
-def multiply():
-    return num1 * num2
-def divide():
-    return num1 / num2
-
-if optr == "+":
-    print("The answer is " + str(add()))
-if optr == "-":
-    print("The answer is " + str(subtract()))
-if optr == "*":
-    print("The answer is " + str(multiply()))
-if optr == "/":
-    print("The answer is " + str(divide()))
-    
-input('Press ENTER to exit: ')
-
+    if optr == "+":
+        print(f"{num1} + {num2} = {add(num1,num2)}")
+        input('Press ENTER Key to run again: ')
+    if optr == "-":
+        print(f"{num1} - {num2} = {subtract(num1,num2)}")
+        input('Press ENTER Key to run again: ')
+    if optr == "*":
+        print(f"{num1} * {num2} = {multiply(num1,num2)}")
+        input('Press ENTER Key to run again: ')
+    if optr == "/":
+        print(f"{num1} / {num2} = {divide(num1,num2)}")
+        input('Press ENTER Key to run again: ')
