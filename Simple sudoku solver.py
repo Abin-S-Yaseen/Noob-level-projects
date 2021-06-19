20a27e3332e52e1bcb97e78cf5c6031e244fd688
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-06-19 23:34:32 +0530

    Update Simple sudoku solver.py

--- Simple sudoku solver.py
+++ Simple sudoku solver.py
@@ -33,3 +33,23 @@ def possible(row,column,number):
         for j in range(0,3):
             if grid[y+i][x+j] == number:
                 return False
+        
+    return True
+
+def solve():
+    global grid
+    for row in range(0,9):
+        for column in range(0,9):
+            if grid[row][column] == 0:
+                for number in range(1,10):
+                    if possible(row,column,number):
+                        grid[row][column] = number
+                        solve()
+                        grid[row][column] = 0
+                return
+    print("The solution is: ")
+    print(np.matrix(grid))
+    input('More solutions? ')
+
+solve()
+input('No more solutions!!! Press ENTER key to exit: ')
