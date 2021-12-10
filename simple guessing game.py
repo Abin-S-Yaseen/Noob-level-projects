7e30ee6f22c445af006272c151917fc80be66ed8
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-12-10 21:10:10 +0530

    Update simple guessing game.py

--- simple guessing game.py
+++ simple guessing game.py
@@ -9,3 +9,4 @@ s = random.randrange(1,16)
 print("Guess a number between 1 and 10")
 confirm1 = input("Press 'Y' to confirm ")
 if confirm1.upper() == 'Y':
+    confirm2 = input(f"Add {p} to the guessed number and press 'Y' to continue ")
