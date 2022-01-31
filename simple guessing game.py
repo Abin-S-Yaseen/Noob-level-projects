3a78c436c6e1348ee2acde79068f665dcd08ff24
Abin-S-Yaseen <abinchingeli12@gmail.com>
2022-01-31 23:33:53 +0530

    Update simple guessing game.py

--- simple guessing game.py
+++ simple guessing game.py
@@ -22,3 +22,4 @@ if confirm1.upper() == 'Y':
                         guessed_num = (int(c) - s)/r + q - p
                         print(f"The number that you guessed in the beginning was {int(guessed_num)} !!!")
                         time.sleep(5)
+                    else:
